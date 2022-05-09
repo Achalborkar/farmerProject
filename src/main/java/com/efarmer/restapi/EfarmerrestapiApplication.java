da7802e9c61f1ebd11cfbb61b33679e85d7f15f0
unknown <achal.borkar@gmail.com>
2022-05-09 17:45:36 +0530

    formercheck

---
+++ src/main/java/com/efarmer/restapi/EfarmerrestapiApplication.java
@@ -0,0 +1,14 @@
+package com.efarmer.restapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EfarmerrestapiApplication {
+
+	public static void main(String[] args) {
+		System.out.println("hello boot");
+		SpringApplication.run(EfarmerrestapiApplication.class, args);
+	}
+}
+
